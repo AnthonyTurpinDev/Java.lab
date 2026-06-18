7f6749d3020168c50d2fb7cf52006f4ba7f93d7b
AnthonyTurpinDev <anthony@example.com>
2026-06-18 09:44:07 +0400

    nouveau dosier

---
+++ src/main/java/lab/exercises/algo/exo2.java
@@ -0,0 +1,11 @@
+package lab.exercises.algo;
+
+ public class exo2 {
+    public static void titre(String[] args) {
+        String mots = "Hello World !"; 
+        System.out.println(mots);
+
+    }
+ }
+
+ 
---
+++ src/main/java/lab/exercises/algo/exo3.java
@@ -0,0 +1,17 @@
+package lab.exercises.algo;
+
+public class exo3 {
+    public static void main(String[] args) {
+        int note = 8;
+        
+        if (note >= 10) {
+            System.out.println("Tu as réussi !");
+        } else {
+            System.out.println("Tu as échoué...");
+        }
+        
+        // Essaie de changer note pour voir le résultat
+        // note = 8;
+    }
+}
+
