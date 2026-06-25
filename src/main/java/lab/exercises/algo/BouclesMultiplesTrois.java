8b6308692a4e31e4671d436306103e850202230b
Anthony Turpin <anthony.tp70@gmail.com>
2026-06-25 18:20:14 +0400

    supprimer exo fait

--- src/main/java/lab/exercises/algo/BouclesMultiplesTrois.java
+++
@@ -1,8 +0,0 @@
-﻿public class BouclesMultiplesTrois {
-    public static void main(String[] args) {
-        // Consigne : affiche les multiples de 3 entre 1 et 50.
-        for ( int  i = 3; i <= 50; i+=3) {
-            System.out.println(i);
-        }
-    }
-}
